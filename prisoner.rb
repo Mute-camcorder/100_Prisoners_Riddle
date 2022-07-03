a588516a018cd57f8ee62de09307e2abd1c2dc2c
Cam <cam@caesium.co.nz>
2022-07-03 16:09:59 +1200

    Optimize the random search method.
    This reduces total runtime for the full simulation by approx half.

--- prisoner.rb
+++ prisoner.rb
@@ -4,21 +4,24 @@ class Prisoner
     end
 
     def found_in_random_search?(room_of_boxes)
-        # Randomly pick half the boxes in the room to search
-        randomly_chosen_indexes = (0...room_of_boxes.length).to_a.sample(room_of_boxes.length / 2)
-        # Open the boxes and see if this prisoner's number was uncovered
-        found_values = randomly_chosen_indexes.map { |x| room_of_boxes[x] }
+        # Randomly pick half the box values in the room to compare against
+        found_values = room_of_boxes.sample(room_of_boxes.length / 2)
         found_values.include? @number
     end
 
     def found_in_cycle?(room_of_boxes)
+        # First, the prisoner visits the box with their own number on it
+        # (guaranteeing they are in a loop containing their number)
         i = @number
-        (room_of_boxes.length / 2).times { 
+        (room_of_boxes.length / 2).times {
+            # Visit the box `i`, and make the value inside our next box to visit
             i = room_of_boxes[i-1]
             if i == @number
+                # Base case, we have found our number inside a box!
                 return true
             end
         }
+        # The cycle containing our number is longer than half the boxes, thus we failed
         return false
     end
 end
